b7e77cf297a1b48a721ba857887d3e489a2c0d00
Divyakanth <64277822+divyakanth07@users.noreply.github.com>
2025-04-11 06:43:10 +0530

    Update sw_receiver.py

--- sw_receiver.py
+++ sw_receiver.py
@@ -1,33 +1,33 @@
 import socket
 
 class SlidReceiver:
-    def __init__(self):
-        self.rws = 8
+    def __init__(self, host='127.0.0.1', port=12345):
         self.rbuf = [None] * 8
         self.rptr = -1
-        self.host = '127.0.0.1'
-        self.port = 12345
+        self.host = host
+        self.port = port
 
     def receive_frames(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((self.host, self.port))
-        
-        while True:
-            nf = int(s.recv(1024).decode().strip())
-            if nf <= self.rws - 1:
-                for i in range(nf):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((self.host, self.port))
+
+            while True:
+                try:
+                    nf = int(s.recv(1024).decode().strip())
+                except:
+                    break
+
+                for _ in range(nf):
                     self.rptr = (self.rptr + 1) % 8
-                    self.rbuf[self.rptr] = s.recv(1024).decode().strip()
-                    print(f"The received Frame {self.rptr} is : {self.rbuf[self.rptr]}")
-                self.rws -= nf
-                print("\nAcknowledgment sent\n")
+                    frame = s.recv(1024).decode().strip()
+                    self.rbuf[self.rptr] = frame
+                    print(f"Received Frame {self.rptr}: {frame}")
+
                 s.send(f"{self.rptr + 1}\n".encode())
-                self.rws += nf
-            else:
-                break
-            ch = s.recv(1024).decode().strip()
-            if ch != "yes":
-                break
+                print("\nAcknowledgment sent\n")
+
+                if s.recv(1024).decode().strip() != "yes":
+                    break
 
 if __name__ == "__main__":
     receiver = SlidReceiver()
